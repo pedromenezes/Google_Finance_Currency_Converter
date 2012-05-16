b4e45e217f5f239014bc2b3d4a8e9cef30331e4e
Pedro Menezes <pedrojudo@gmail.com>
2012-05-16 15:06:43 -0300

    adds new test for integer response via google

--- spec/google_finance_currency_converter_spec.rb
+++ spec/google_finance_currency_converter_spec.rb
@@ -1,13 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "GoogleFinanceCurrencyConverter" do
-  it "converts 1 british pound to brazilian reais" do
-    File.open(File.expand_path(File.dirname(__FILE__) + '/helper/mock.html'), 'r') do |f|
-      stub_request(:get, "http://www.google.com/finance/converter?a=1&from=GBP&to=BRL").
-          to_return(:body => f)
-    end
+  it "converts integer to brazilian reais" do
+    stub_converted_val(2)
+
+    converter = GoogleFinanceCurrencyConverter.new(:from => 'GBP', :to => 'BRL')
+    converter.value.should == 2
+  end
+
+  it "converts floats to brazilian reais" do
+    stub_converted_val(2.784)
 
     converter = GoogleFinanceCurrencyConverter.new(:from => 'GBP', :to => 'BRL')
     converter.value.should == 2.78
+  end
+end
+
+def stub_converted_val(val)
+  File.open(File.expand_path(File.dirname(__FILE__) + '/helper/response.html'), 'r') do |f|
+    stub_request(:get, "http://www.google.com/finance/converter?a=1&from=GBP&to=BRL").
+        to_return(:body => f.read.gsub("<converted_val>", val.to_s))
   end
 end
